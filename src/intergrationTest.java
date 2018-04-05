bc74cb7d008fe6e72ec560d2a723bbd56f4deffd
Steven <stevenlin0918@gmail.com>
2018-04-05 18:00:37 +0800

    add intergration test sample

---
+++ src/intergrationTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class intergrationTest {
+	UI ui;
+	ByteArrayOutputStream outContent;
+	@Before
+	public void setUp() throws Exception {
+		//ui = new UI();
+		outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		ui= null;
+	}
+
+	@Test
+	public void test1() {
+		System.setIn(new ByteArrayInputStream("975002021\r\n".getBytes()));
+		String testStr1 = 
+				"Please enter your student ID to start or enter Q to quit :\r\n"+
+				"Student with ID 975002021 has Lab1 grade 81\r\n" + 
+				"Student with ID 975002021 has Lab2 grade 97\r\n" + 
+				"Student with ID 975002021 has Lab3 grade 90\r\n" + 
+				"Student with ID 975002021 Midterm grade 82\r\n" + 
+				"Student with ID 975002021 Final Exam grade 84\r\n" + 
+				"Student with ID 975002021 has totalgrade 85\r\n";
+		assertEquals(testStr1,outContent.toString());
+	}
+
+}
